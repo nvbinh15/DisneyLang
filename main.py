8b319467426763618438faa1f3ec0bc1006a6e79
pbthang <bathang02@gmail.com>
2022-01-07 20:19:34 +0800

    finish profanity filtering

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 
 from cogs.voice import Voice
 from cogs.add import Add
+from cogs.insult import Insult
+from cogs.filter import Filter
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
 
@@ -12,8 +14,11 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
 async def on_ready():
     print(f"Logged in as {bot.user} {bot.user.id}")
 
+
 bot.add_cog(Add(bot))
 bot.add_cog(Voice(bot))
+bot.add_cog(Insult(bot))
+bot.add_cog(Filter(bot))
 
 log()    
 bot.run(os.environ['TOKEN'])
