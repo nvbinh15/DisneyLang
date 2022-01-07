43784242e6d4fac6d60f3e8d40776eb3110290ab
Thao <tranthaoptcnn@gmail.com>
2022-01-07 15:11:02 +0800

    trivia

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 import discord
 from log import log
 import os
+import random
+import asyncio
+import requests
+import json
 
-client = discord.Client()
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
-@client.event
-async def on_ready():
-    print(f"Logged in as {client.user}.")
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('$task'):
+            await message.channel.send('Answer True or False to turn off annoying sound:')
+            response = requests.get("https://opentdb.com/api.php?amount=1&type=boolean")
+            jprint(response.json())
+            result = response.json()['results']
+            print(result)
+            question = result[0]['question']
+            question = question.replace("&quot;", "'")
+            answer = result[0]['correct_answer']
+            print(question)
+            await message.channel.send(question)
+
+            def is_correct(m):
+                return m.author == message.author
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=10.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send('Sorry, you took too long it was {}.'.format(answer))
+
+            if guess.content.lower() == answer.lower():
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send('Oops. It is actually {}.'.format(answer))
+
+client = MyClient()
 
 
 log()    
